8a85f0f26b7a5887deefe618cc34a3fda1c389c2
Andy Whitfield <andy@andywhitfield.me.uk>
2023-01-22 15:12:57 +0000

    Little bit more work on the cli project

--- SmallSafe.Cli/CliService.cs
+++ SmallSafe.Cli/CliService.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.Extensions.Logging;
 using SmallSafe.Secure.Model;
@@ -18,6 +19,7 @@ public class CliService
 
     public async Task ExecuteAsync(string? safeDbFile)
     {
+        var masterPassword = "";
         var firstPass = true;
         while (true)
         {
@@ -31,7 +33,7 @@ public class CliService
 
             _logger.LogInformation("1. Choose an existing Safe DB file");
             _logger.LogInformation("2. Create a new Safe DB file");
-            _logger.LogInformation("0. Exit");
+            _logger.LogInformation("x. Exit");
 
             var selection = Console.ReadLine();
             switch (selection?.Trim())
@@ -46,7 +48,7 @@ public class CliService
 
                     if (string.IsNullOrEmpty(safeDbFile))
                     {
-                        _logger.LogInformation("No file entered, try again.");
+                        _logger.LogWarning("No file entered, try again.");
                         firstPass = true;
                         safeDbFile = null;
                         break;
@@ -54,7 +56,7 @@ public class CliService
 
                     if (File.Exists(safeDbFile))
                     {
-                        _logger.LogInformation("File already exists. To create a new Safe DB file, please enter a new filename. Or choose option 1 to open an existing Safe DB file.");
+                        _logger.LogWarning("File already exists. To create a new Safe DB file, please enter a new filename. Or choose option 1 to open an existing Safe DB file.");
                         firstPass = true;
                         safeDbFile = null;
                         break;
@@ -62,29 +64,47 @@ public class CliService
 
                     _logger.LogInformation("Enter a new master password. Ensure it is a strong password - ideally a long passphrase using a combination of uppercase and lowercase letters, numbers, and punctuation.");
                     ConsoleKeyInfo key;
-                    StringBuilder masterPassword = new();
+                    StringBuilder enteredMasterPassword = new();
                     while ((key = Console.ReadKey(true)).Key != ConsoleKey.Enter)
                     {
-                        masterPassword.Append(key.KeyChar);
+                        enteredMasterPassword.Append(key.KeyChar);
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                    masterPassword = enteredMasterPassword.ToString();
+
+                    if (masterPassword.Length == 0)
+                    {
+                        _logger.LogWarning("No master password entered, try again.");
+                        firstPass = true;
+                        safeDbFile = null;
+                        break;
+                    }
+
+                    _logger.LogInformation("Re-type your new master password:");
+                    enteredMasterPassword.Clear();
+                    while ((key = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+                    {
+                        enteredMasterPassword.Append(key.KeyChar);
                         Console.Write('*');
                     }
                     Console.WriteLine();
 
-                    if (masterPassword.Length == 0)
+                    if (masterPassword != enteredMasterPassword.ToString())
                     {
-                        _logger.LogInformation("No master password entered, try again.");
+                        _logger.LogWarning("Master passwords do not match, try again.");
                         firstPass = true;
                         safeDbFile = null;
                         break;
                     }
 
                     {
-                        using FileStream fileStream = new(safeDbFile, FileMode.CreateNew, FileAccess.Write);
-                        await _safeDbService.WriteAsync(masterPassword.ToString(), Enumerable.Empty<SafeGroup>(), fileStream);
+                        await using FileStream fileStream = new(safeDbFile, FileMode.CreateNew, FileAccess.Write);
+                        await _safeDbService.WriteAsync(masterPassword, Enumerable.Empty<SafeGroup>(), fileStream);
                     }
 
                     break;
-                case "0":
+                case "x":
                     return;
                 default:
                     _logger.LogWarning("Unknown option, try again");
@@ -96,6 +116,91 @@ public class CliService
 
         _logger.LogInformation($"Using password safe db {safeDbFile}...");
 
-        // TODO: prompt for master password (unless we've created a new DB) and show the password groups
+        if (string.IsNullOrEmpty(masterPassword))
+        {
+            while (true)
+            {
+                _logger.LogInformation("Enter the master password for the Safe DB:");
+                ConsoleKeyInfo key;
+                StringBuilder enteredMasterPassword = new();
+                while ((key = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+                {
+                    enteredMasterPassword.Append(key.KeyChar);
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                masterPassword = enteredMasterPassword.ToString();
+
+                if (masterPassword.Length == 0)
+                {
+                    _logger.LogWarning("No master password entered, try again.");
+                    continue;
+                }
+
+                try
+                {
+                    await using FileStream fileStream = new(safeDbFile, FileMode.Open, FileAccess.Read);
+                    await _safeDbService.ReadAsync(masterPassword, fileStream);
+                }
+                catch (CryptographicException)
+                {
+                    _logger.LogWarning("Cannot read Safe DB using the given password, try again");
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        await ManageSafeDbAsync(safeDbFile, masterPassword);
+    }
+
+    private async Task ManageSafeDbAsync(string safeDbFile, string masterPassword)
+    {
+        while (true)
+        {
+            IEnumerable<SafeGroup> safeGroups;
+            {
+                await using FileStream fileStream = new(safeDbFile, FileMode.Open, FileAccess.Read);
+                safeGroups = await _safeDbService.ReadAsync(masterPassword, fileStream);
+            }
+
+            _logger.LogInformation("Groups:");
+            if (!safeGroups.Any())
+            {
+                _logger.LogInformation(" <no groups>");
+            }
+            else
+            {
+                var groupNum = 1;
+                foreach (var safeGroup in safeGroups)
+                    _logger.LogInformation($"{groupNum++}. {safeGroup.Name}");
+            }
+
+            _logger.LogInformation("");
+            _logger.LogInformation("n. Create a new group");
+            _logger.LogInformation("x. Exit");
+
+            var selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "n":
+                    // TODO: create a new group
+                    break;
+                case "x":
+                    return;
+                default:
+                    // is it a valid group number
+                    if (int.TryParse(selection, out var groupNum) && groupNum > 0 && groupNum <= safeGroups.Count())
+                    {
+                        // show group
+                    }
+                    else
+                    {
+                        _logger.LogWarning(@"Unknown option");
+                    }
+                    break;
+            }
+        }
     }
 }
--- SmallSafe.Secure/Services/ISafeDbService.cs
+++ SmallSafe.Secure/Services/ISafeDbService.cs
@@ -4,6 +4,7 @@ namespace SmallSafe.Secure.Services;
 
 public interface ISafeDbService
 {
+    /// <exception cref="CryptographicException">Thrown if the master password can't read from the input stream.</exception>
     Task<IEnumerable<SafeGroup>> ReadAsync(string masterPassword, Stream inputStream);
     Task WriteAsync(string masterPassword, IEnumerable<SafeGroup> safeGroups, Stream outputStream);
 }
