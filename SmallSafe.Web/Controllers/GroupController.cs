0027af3dd53a95a2acfc03ce46e756c31ec04422
Andy Whitfield <andy@andywhitfield.me.uk>
2023-03-19 17:00:01 +0000

    WIP checkpoint group page

---
+++ SmallSafe.Web/Controllers/GroupController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmallSafe.Web.ViewModels.Group;
+
+namespace SmallSafe.Web.Controllers;
+
+[Authorize]
+public class GroupController : Controller
+{
+    [HttpGet("~/group/{groupId}")]
+    public IActionResult Index(int groupId)
+    {
+        return View(new IndexViewModel(HttpContext));
+    }
+}
---
+++ SmallSafe.Web/ViewModels/Group/IndexViewModel.cs
@@ -0,0 +1,8 @@
+namespace SmallSafe.Web.ViewModels.Group;
+
+public class IndexViewModel : BaseViewModel
+{
+    public IndexViewModel(HttpContext context) : base(context)
+    {
+    }
+}
