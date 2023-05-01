394ddf088375d49b582b202d19ad4b2d30b6154c
Andy Whitfield <andy@andywhitfield.me.uk>
2023-05-01 10:42:56 +0100

    Added decrypt & copy to clipboard

--- SmallSafe.Web/wwwroot/js/smallsafe.js
+++ SmallSafe.Web/wwwroot/js/smallsafe.js
@@ -38,3 +38,73 @@ function ssInitialise() {
         }
     });
 }
+
+function ssInitialiseSafeEntry() {
+    $('.ss-visible-value').hide();
+    $('button.ss-hidden-value:first-child').click(function() {
+        let $entryGroup = $(this).parentsUntil('li.ss-list-item').parent();
+        let $groupId = $entryGroup.attr('data-group');
+        let $entryId = $entryGroup.attr('data-entry');
+
+        $.getJSON('/api/group/' + $groupId + '/entry/' + $entryId)
+            .done(function(data) {
+                $('textarea', $entryGroup).val(data.value);
+                $('.ss-hidden-value', $entryGroup).hide();
+                $('.ss-visible-value', $entryGroup).show();
+            })
+            .fail(function() { window.location.reload(); });
+    });
+    $('button.ss-hidden-value:nth-last-child(2)').click(function() {
+        let $entryGroup = $(this).parentsUntil('li.ss-list-item').parent();
+        let $groupId = $entryGroup.attr('data-group');
+        let $entryId = $entryGroup.attr('data-entry');
+
+        $.getJSON('/api/group/' + $groupId + '/entry/' + $entryId)
+            .done(function(data) {
+                $('textarea', $entryGroup).val(data.value);
+                $('button.ss-hidden-value:nth-last-child(2)', $entryGroup).hide();
+                $('button.ss-visible-value:last-child', $entryGroup).show();
+            })
+            .fail(function() { window.location.reload(); });
+    });
+    $('button.ss-visible-value:nth-child(2)').click(function() {
+        let $entryGroup = $(this).parentsUntil('li.ss-list-item');
+        $('.ss-visible-value', $entryGroup).hide();
+        $('textarea', $entryGroup).val('');
+        $('.ss-hidden-value', $entryGroup).show();
+    });
+    $('button.ss-visible-value:last-child').click(function() {
+        let $entryGroup = $(this).parentsUntil('li.ss-list-item');
+        ssCopyToClipboard($('textarea', $entryGroup).val());
+        $('.ss-visible-value', $entryGroup).hide();
+        $('textarea', $entryGroup).val('');
+        $('.ss-hidden-value', $entryGroup).show();
+    });
+}
+
+function ssCopyToClipboard(text) {
+    if (!navigator.clipboard) {
+        ssFallbackCopyToClipboard(text)
+    } else {
+        navigator.clipboard.writeText(text)
+    }
+}
+
+function ssFallbackCopyToClipboard(text) {
+    let textArea = document.createElement("textarea")
+    textArea.value = text
+    textArea.style.top = "0"
+    textArea.style.left = "0"
+    textArea.style.position = "fixed"
+    document.body.appendChild(textArea)
+    textArea.focus()
+    textArea.select()
+
+    try {
+        document.execCommand('copy')
+    } catch (err) {
+        console.error('Unable to copy', err)
+    }
+
+    document.body.removeChild(textArea)
+}
