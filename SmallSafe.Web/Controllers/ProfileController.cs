f469ea77654e606192ec8447ee5621b3649d4c80
Andy Whitfield <andy@andywhitfield.me.uk>
2023-05-14 18:06:12 +0100

    Finished profile page; handle master password change

--- SmallSafe.Web/Controllers/ProfileController.cs
+++ SmallSafe.Web/Controllers/ProfileController.cs
@@ -2,6 +2,7 @@ using Dropbox.Api;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using SmallSafe.Secure.Model;
 using SmallSafe.Web.Authorization;
 using SmallSafe.Web.Data;
 using SmallSafe.Web.Services;
@@ -15,19 +16,55 @@ public class ProfileController : Controller
     private readonly ILogger<ProfileController> _logger;
     private readonly IOptionsSnapshot<DropboxConfig> _dropboxConfig;
     private readonly IUserService _userService;
+    private readonly IAuthorizationSession _authorizationSession;
+    private readonly ISafeDbReadWriteService _safeDbReadWriteService;
+    private readonly ITwoFactor _twoFactor;
 
-    public ProfileController(ILogger<ProfileController> logger, IOptionsSnapshot<DropboxConfig> dropboxConfig, IUserService userService)
+    public ProfileController(
+        ILogger<ProfileController> logger,
+        IOptionsSnapshot<DropboxConfig> dropboxConfig,
+        IUserService userService,
+        IAuthorizationSession authorizationSession,
+        ISafeDbReadWriteService safeDbReadWriteService,
+        ITwoFactor twoFactor)
     {
         _logger = logger;
         _dropboxConfig = dropboxConfig;
         _userService = userService;
+        _authorizationSession = authorizationSession;
+        _safeDbReadWriteService = safeDbReadWriteService;
+        _twoFactor = twoFactor;
     }
 
     [HttpGet("~/profile")]
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index([FromQuery] bool? passwordUpdated)
     {
         var user = await _userService.GetUserAsync(User);
-        return View(new IndexViewModel(HttpContext, user.IsConnectedToDropbox));
+        return View(new IndexViewModel(HttpContext, user.IsConnectedToDropbox, passwordUpdated: passwordUpdated ?? false));
+    }
+
+    [HttpPost("~/profile/password")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateMasterPassword([FromForm] string? currentpassword, [FromForm] string? newpassword, [FromForm] string? twofa)
+    {
+        var user = await _userService.GetUserAsync(User);
+        IEnumerable<SafeGroup>? groups;
+        if (!string.IsNullOrEmpty(currentpassword) &&
+            !string.IsNullOrEmpty(newpassword) &&
+            (groups = await _safeDbReadWriteService.TryReadGroupsAsync(user, currentpassword)) != null &&
+            _twoFactor.ValidateTwoFactorCodeForUser(user, twofa))
+        {
+            _logger.LogInformation("Current password & 2fa code valid, updating master password");
+            await _safeDbReadWriteService.WriteGroupsAsync(user, newpassword, groups);
+            await _userService.LoginSuccessAsync(user);
+            _authorizationSession.Validate(newpassword);
+
+            return Redirect("~/profile?passwordUpdated=true");
+        }
+
+        await _userService.LoginFailureAsync(user);
+        _logger.LogWarning("Bad master password or invalid 2fa code, returning to profile page and password won't be updated");
+        return View("Index", new IndexViewModel(HttpContext, user.IsConnectedToDropbox, true));
     }
 
     [HttpPost("~/profile/dropbox-connect")]
--- SmallSafe.Web/ViewModels/Profile/IndexViewModel.cs
+++ SmallSafe.Web/ViewModels/Profile/IndexViewModel.cs
@@ -2,10 +2,15 @@ namespace SmallSafe.Web.ViewModels.Profile;
 
 public class IndexViewModel : BaseViewModel
 {
-    public IndexViewModel(HttpContext context, bool isConnectedToDropbox) : base(context)
+    public IndexViewModel(HttpContext context, bool isConnectedToDropbox,
+        bool isPasswordOrTwoFaInvalid = false, bool passwordUpdated = false) : base(context)
     {
         IsConnectedToDropbox = isConnectedToDropbox;
+        IsPasswordOrTwoFaInvalid = isPasswordOrTwoFaInvalid;
+        PasswordUpdated = passwordUpdated;
     }
 
     public bool IsConnectedToDropbox { get; init; }
+    public bool IsPasswordOrTwoFaInvalid { get; init; }
+    public bool PasswordUpdated { get; init; }
 }
