294a4d214e883251f5d7a5216aba66044413c36b
Andy Whitfield <andy@andywhitfield.me.uk>
2026-02-19 17:17:33 +0000

    Added new db property to store the new safedb format
    Plan to add history / auditing and compression to reduce the db size

--- SmallSafe.Web/Data/Models/UserAccount.cs
+++ SmallSafe.Web/Data/Models/UserAccount.cs
@@ -15,13 +15,14 @@ public class UserAccount
     public DateTime? LastTwoFactorFailure { get; set; }
     public int TwoFactorFailureCount { get; set; }
     public string? SafeDb { get; set; }
+    public byte[]? EncyptedSafeDb { get; set; }
     public string? DropboxAccessToken { get; set; }
     public string? DropboxRefreshToken { get; set; }
 
     public bool IsAccountConfigured =>
         DeletedDateTime == null &&
         !string.IsNullOrEmpty(TwoFactorKey) &&
-        !string.IsNullOrEmpty(SafeDb);
+        (!string.IsNullOrEmpty(SafeDb) || EncyptedSafeDb != null);
     
     public bool IsConnectedToDropbox =>
         !string.IsNullOrEmpty(DropboxAccessToken) &&
---
+++ SmallSafe.Web/Migrations/20260219171246_AddedBinarySafeDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmallSafe.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBinarySafeDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "EncyptedSafeDb",
+                table: "UserAccounts",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncyptedSafeDb",
+                table: "UserAccounts");
+        }
+    }
+}
